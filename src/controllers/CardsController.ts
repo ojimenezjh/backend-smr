55d0a33f244056cde978a35a4d017f82108d10e1
ojimenezjh <ojimenezjh@uoc.edu>
2020-12-04 22:45:11 +0100

    ok

--- src/controllers/CardsController.ts
+++ src/controllers/CardsController.ts
@@ -4,35 +4,73 @@ import { QueryResult } from 'pg';
 
 
 class CardsController {
-    public async getCards(req: Request, res: Response) : Promise<Response> { // La promesa sirve para decir que si o si la función tiene que recibir una respuesta
+    public async getCards(req: Request, res: Response): Promise<Response> { // La promesa sirve para decir que si o si la función tiene que recibir una respuesta
 
-        try{
+        try {
             const pool = poolaso();
             console.log(database_usuario());
             const response: QueryResult = await pool.query('SELECT * FROM comida.cartas ORDER BY posicion');
             return res.status(200).json(response.rows);
         }
-    
-        catch(e){
+
+        catch (e) {
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
-        
+
     };
-    
-    public async getCardbyId(req: Request, res: Response) : Promise<Response> {
+
+    public async getCardbyId(req: Request, res: Response): Promise<Response> {
         try {
+            const pool = poolaso();
+            const id = parseInt(req.params.id);
+            const response: QueryResult = await pool.query('SELECT * FROM comida.cartas WHERE id_carta = $1', [id]);
+            return res.json(response.rows);
+        }
+        catch (e) {
+            console.log(e);
+            return res.status(500).json('Internal Server Error');
+        }
+    };
+
+    public async createCard(req: Request, res: Response): Promise<Response> {
+        const pool = poolaso();
+        const { id, nombre, descripcion, hora_inicio, hora_fin, imagen } = req.body;
+        const response: QueryResult = await pool.query('INSERT INTO comida.cartas (id, nombre, descripcion, hora_inicio, hora_fin, imagen, posicion) VALUES ($1, $2, $3, $4, $5, (SELECT MAX(posicion)+1 FROM comida.cards))', [id, nombre, descripcion, hora_inicio, hora_fin, imagen]);
+        return res.json({
+            message: 'Card created succesfully',
+            body: {
+                card: {
+                    id,
+                    nombre,
+                    descripcion,
+                    hora_inicio,
+                    hora_fin,
+                    imagen
+                }
+            }
+        });
+    };
+
+    public async updateCard(req: Request, res: Response): Promise<Response> {
         const pool = poolaso();
         const id = parseInt(req.params.id);
-        const response: QueryResult = await pool.query('SELECT * FROM comida.cartas WHERE id_carta = $1', [id]);
-        return res.json(response.rows);
-        }
-        catch(e){
-            console.log(e);
-            return res.status(500).json('Internal Server Error');
-        }
+        const { nombre, descripcion, hora_inicio, hora_fin, imagen } = req.body;
+        const response: QueryResult = await pool.query('UPDATE comida.cartas SET nombre = $1, descripcion = $2, hora_inicio = $3, hora_fin= $4, imagen = $5 WHERE id = $6', [nombre, descripcion, hora_inicio, hora_fin, imagen, id]);
+        return res.json({
+            message: `Card ${id} updated succesfully`
+        });
     };
-    
+
+    public async deleteCard(req: Request, res: Response): Promise<Response> {
+        const pool = poolaso();
+        const id = parseInt(req.params.id);
+        await pool.query('DELETE FROM comida.cartas WHERE id = $1', [id]);
+        return res.json({
+            message: `Card ${id} deleted succesfully`
+        });
+    };
+
 }
 
 const cardsController = new CardsController();
--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -6,87 +6,87 @@ import { QueryResult, Pool } from 'pg';
 class ProductsController {
 
 
-    public async getProducts(req: Request, res: Response) : Promise<Response> { // La promesa sirve para decir que si o si la función tiene que recibir una respuesta
+    public async getProducts(req: Request, res: Response): Promise<Response> { // La promesa sirve para decir que si o si la función tiene que recibir una respuesta
 
-        try{
+        try {
             const pool = poolaso();
             console.log(database_usuario());
             const response: QueryResult = await pool.query('SELECT * FROM comida.productos ORDER BY id_producto');
             return res.status(200).json(response.rows);
         }
-    
-        catch(e){
+
+        catch (e) {
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
-        
+
     };
-    
-    public async getProductbyId(req: Request, res: Response) : Promise<Response> {
+
+    public async getProductbyId(req: Request, res: Response): Promise<Response> {
         try {
-        const pool = poolaso();
-        const id_producto = parseInt(req.params.id_producto);
-        const response: QueryResult = await pool.query('SELECT * FROM comida.productos WHERE id_producto = $1', [id_producto]);
-        return res.json(response.rows);
+            const pool = poolaso();
+            const id_producto = parseInt(req.params.id_producto);
+            const response: QueryResult = await pool.query('SELECT * FROM comida.productos WHERE id_producto = $1', [id_producto]);
+            return res.json(response.rows);
         }
-        catch(e){
+        catch (e) {
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
     };
 
-    public async getProductbyCard(req: Request, res: Response) : Promise<Response> {
+    public async getProductbyCard(req: Request, res: Response): Promise<Response> {
         try {
-        const pool = poolaso();
-        const id_carta = (req.params.id_carta);
-        const response: QueryResult = await pool.query('SELECT p.* FROM comida.productos p NATURAL JOIN comida.carta_producto WHERE id_carta = $1', [id_carta]);
-        console.log(id_carta)
-        console.log(response.rows)
-        console.log('consulta realizada')
-        return res.json(response.rows);
+            const pool = poolaso();
+            const id_carta = (req.params.id_carta);
+            const response: QueryResult = await pool.query('SELECT p.* FROM comida.productos p NATURAL JOIN comida.carta_producto WHERE id_carta = $1', [id_carta]);
+            console.log(id_carta)
+            console.log(response.rows)
+            console.log('consulta realizada')
+            return res.json(response.rows);
         }
-        catch(e){
-            
+        catch (e) {
+
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
     };
 
-    public async insertProductsByCard(req: Request, res: Response) : Promise<Response> {
+    public async insertProductsByCard(req: Request, res: Response): Promise<Response> {
         try {
-        const pool = poolaso();
-        const id_carta = req.body.id_carta;
-        const id_producto = req.body.id_producto;
-        var id_products;
-        id_products = JSON.parse(id_producto);         
-        const response: QueryResult = await pool.query('SELECT * FROM comida.addproductstocard($1,$2)', [id_carta, id_products]);
-        console.log(id_carta)
-        //console.log(id_productos)
-        return res.json({
-            message: 'Products added succesfully'
-        });
+            const pool = poolaso();
+            const id_carta = req.body.id_carta;
+            const id_producto = req.body.id_producto;
+            var id_products;
+            id_products = JSON.parse(id_producto);
+            const response: QueryResult = await pool.query('SELECT * FROM comida.addproductstocard($1,$2)', [id_carta, id_products]);
+            console.log(id_carta)
+            //console.log(id_productos)
+            return res.json({
+                message: 'Products added succesfully'
+            });
         }
-        catch(e){
-            
+        catch (e) {
+
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
     };
 
-    public async getProductbyName(req: Request, res: Response) : Promise<Response> {   
-        try{
-        const pool = poolaso();
-        const producto = (req.params.producto)
-        const response: QueryResult = await pool.query('SELECT * FROM comida.search_bar_producto($1)', [producto]);
-        console.log(producto)
-        return res.json(response.rows);
+    public async getProductbyName(req: Request, res: Response): Promise<Response> {
+        try {
+            const pool = poolaso();
+            const producto = (req.params.producto)
+            const response: QueryResult = await pool.query('SELECT * FROM comida.search_bar_producto($1)', [producto]);
+            console.log(producto)
+            return res.json(response.rows);
         }
-        catch(e){
+        catch (e) {
             console.log(e);
             return res.status(500).json('Internal Server Error');
         }
     };
-    
+
 }
 
 const productsController = new ProductsController();
