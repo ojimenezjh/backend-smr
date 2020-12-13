34d5aad60c8bbbdf3bee6843956c15697ad06fa4
ojimenezjh <ojimenezjh@uoc.edu>
2020-12-13 18:33:37 +0100

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { json, Application, Router } from 'express';
-const fileUpload = require('express-fileupload')
+//const fileUpload = require('express-fileupload')
 import morgan from 'morgan';
 import cors from 'cors';
 import usersController  from './controllers/UsersController';
@@ -36,7 +36,7 @@ class Server {
         this.app.use(cors()); // Nos configura api
         this.app.use(json()); // Servidor entiende archivos json
         this.app.use(express.urlencoded({extended: false})); // Servidor puede transformar datos formularios html a json
-        this.app.use(fileUpload());
+        //this.app.use(fileUpload());
  
     }
 
@@ -53,13 +53,13 @@ class Server {
         this.app.use('/', corpRoutes);
         this.app.use('/', usersRoutes);
         this.app.use('/', mesasRoutes);
-        this.app.post('/upload',(req,res) => {
+    /*     this.app.post('/upload',(req,res) => {
             let EDFile = req.body;
             EDFile.mv(`./files/${EDFile.name}`,err => {
                 if(err) return res.status(500).send({ message : err })
                 return res.status(200).send({ message : 'File upload' })
             })
-        })
+        }) */
 
     }
     start(): void {
